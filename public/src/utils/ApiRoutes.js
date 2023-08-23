64245048328eacc067f8b8a8d8c8c8c6278215ea
nraut6090 <140501578+nraut6090@users.noreply.github.com>
2023-08-23 17:03:14 +0530

    Update ApiRoutes.js

--- public/src/utils/ApiRoutes.js
+++ public/src/utils/ApiRoutes.js
@@ -1,7 +1,7 @@
-export const host="http://localhost:5000";
-export const registerRoute=`https://chat-app-final-api.vercel.app/api/auth/register`;
-export const loginRoute=`https://chat-app-final-api.vercel.app/api/auth/login`;
-export const setAvatarRoute=`https://chat-app-final-api.vercel.app/api/auth/setAvatar`;
-export const allUsersRoute = `https://chat-app-final-api.vercel.app/api/auth/allusers`;
-export const sendMessageRoute=`https://chat-app-final-api.vercel.app/api/messages/addmsg`;
-export const getAllMessagesRoute =`https://chat-app-final-api.vercel.app/api/messages/getmsg`;
+export const host="https://chat-app-final-api.vercel.app";
+export const registerRoute=`${host}/api/auth/register`;
+export const loginRoute=`${host}/api/auth/login`;
+export const setAvatarRoute=`${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute=`${host}/api/messages/addmsg`;
+export const getAllMessagesRoute =`${host}/api/messages/getmsg`;
